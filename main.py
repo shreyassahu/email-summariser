f9f3d858c61ff08573bbc3cacb29af147a69128f
Shreyas Sahu <sahu@Shreyass-MacBook-Pro.local>
2026-05-13 13:17:56 -0700

    Async & other changes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Request
 from email_pydantic import Email
 from claude import call_claude
-import requests
 import httpx
 import os
 from dotenv import load_dotenv
 
 app = FastAPI()
+load_dotenv()
+
 
 slack_url = os.environ.get("SLACK_URL")
 
@@ -20,5 +21,6 @@ async def webhook(request: Request):
     email = Email(TextBody=body["TextBody"], From=body["From"], Subject=body["Subject"])
     ai_response = await call_claude(email.TextBody)
     print(f"AI Response: {ai_response.content[0].text}")
-    r = httpx.post(slack_url, json={"text": ai_response.content[0].text})
+    async with httpx.AsyncClient() as client:
+        await client.post(slack_url, json={"text": ai_response.content[0].text})
     return {"received" : True}
